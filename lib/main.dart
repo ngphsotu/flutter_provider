4f6272c22e126dc8391ee440ec1910f8faa8a0c1
Nguyen Phuc Son Tung <52483128+ngphsotu@users.noreply.github.com>
2022-08-27 20:08:17 +0700

    flutter Provider

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(
+      ChangeNotifierProvider(
+        create: (_) => CounterProvider(),
+        child: const MyApp(),
+      ),
+    );
+
+class CounterProvider extends ChangeNotifier {
+  int _counter = 0;
+  int get counter => _counter;
+  void add() {
+    _counter++;
+    notifyListeners();
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Provier',
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: const CounterHome(),
+    );
+  }
+}
+
+class CounterHome extends StatelessWidget {
+  const CounterHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter Provider')),
+      body: Center(
+        child: Text(
+          context.watch<CounterProvider>().counter.toString(),
+          style: const TextStyle(fontSize: 50),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          context.read<CounterProvider>().add();
+        },
+      ),
+    );
+  }
+}
+
+/*
+  // Nhà cung cấp
+    class YourProvider extends ChangeNotifier
+    notifyListeners();
+  --------------------------------------------------
+  // Nhà phân phối
+    ChangNotifierProvider
+    MultiProvider
+  --------------------------------------------------
+  // Người sử dụng
+    Consumer<YourProvider>
+    context.watch<YourProvider>()
+    context.read<YourProvider>()
+  --------------------------------------------------
+*/
+
